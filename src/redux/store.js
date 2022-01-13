22f8aaf07e490de53587b208b31982cc7bb06227
Brahma behera <75154554+ironman2211@users.noreply.github.com>
2022-01-13 16:28:11 +0530

    updating the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './root-reduser';
-const middleware = [logger];
+const middleware = [];
 if (process.env.NODE_ENV === 'development') {
   middleware.push(logger);
 }
