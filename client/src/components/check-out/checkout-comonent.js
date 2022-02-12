028f18af77c9f12f82cbbd40a713cdacd3a0f7cc
brahmananda behera <beherabrahmananda77@gmail.com>
2022-02-11 22:22:27 -0800

    separating our cra app into client server,added express server

--- client/src/components/check-out/checkout-comonent.js
+++ client/src/components/check-out/checkout-comonent.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-
+import axios from 'axios';
 const StripeCheckoutButton = ({ amount }) => {
   const publishableKey =
     'pk_test_51KFzknSJ74jQLJ2iacDeA6dv9zt0AiPwFl4Gyla8H7CAc514CbB1xWJnpQYMr9Hz7qO2w8dHJe3S978nUZ81QDNK006RPHWBSy';
   const PriceForStripe = amount * 100;
   const onToken = token => {
-    console.log(token);
-    alert('payment sucesfull');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: PriceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
   };
   return (
     <StripeCheckout
